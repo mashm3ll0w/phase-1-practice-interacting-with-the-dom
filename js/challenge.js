06c33262fc081c25c4a45e526b75768d75a25e91
Charles Swaleh <charlswall@live.com>
2022-12-18 16:05:51 +0300

    Create the function to add comments

--- js/challenge.js
+++ js/challenge.js
@@ -19,6 +19,16 @@ function plusButton(){
   domTimer.textContent = currentTime
 }
 
+function addComment(e){
+  e.preventDefault()
+  if(e.target["comment-input"].value !== ""){
+    const commentList = document.createElement("ul")
+    const comment = `<li>${e.target["comment-input"].value}</li>`
+    commentList.innerHTML += comment
+    document.getElementById("list").appendChild(commentList)
+    e.target["comment-input"].value = ""
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   
@@ -26,4 +36,5 @@ document.addEventListener("DOMContentLoaded", () => {
   
   document.getElementById("minus").addEventListener("click", minusButton)
   document.getElementById("plus").addEventListener("click", plusButton)
+  document.getElementById("comment-form").addEventListener("submit", addComment)
 })
